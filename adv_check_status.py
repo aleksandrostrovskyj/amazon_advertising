26d09b3b4f8232efd3a9836097cbc28d3d6ac91e
aleksandrostrovskyj <aleksandr.ostrovskyj@gmail.com>
2020-02-22 17:31:23 +0200

    Add report_date to responses data

--- adv_check_status.py
+++ adv_check_status.py
@@ -4,12 +4,15 @@ import asyncio
 import logging
 # User modules
 from amazon_advertise import Reports
-from connectors import redis_pool
+from connectors import redis_connections_pool
+
+# TODO - collect data from the queue in a bundle and implement asyncio.gather()?
+# TODO - Possible negative impact: high probability of receiving too many requests response, check later
 
 
 async def main():
     amz = Reports()
-    pool = await redis_pool()
+    pool = await redis_connections_pool()
 
     redis_list = {
         'SUCCESS': 'targets:ready',
@@ -31,13 +34,18 @@ async def main():
 
             data_json = json.loads(data)
             report_id = data_json['reportId']
+            report_date = data_json['report_date']
+            report_type = data_json['recordType']
 
             async with aiohttp.ClientSession() as session:
                 response = await amz.status(session=session, report_id=report_id)
                 response_json = await response.json()
 
+            # Add report date and report type
+            response_json.update({'report_date': report_date, 'recordType': report_type})
             report_status = response_json['status']
             logging.info(f'Report {report_id} has status {report_status}; Push to redis_list{[report_status]}')
+            # Add to the redis pool (depend on status) serialized json string
             await conn.execute('RPUSH', redis_list[report_status], json.dumps(response_json))
 
 
--- adv_request_report.py
+++ adv_request_report.py
@@ -6,29 +6,40 @@ from settings import metrics
 from datetime import datetime, date, timedelta
 # User modules
 from amazon_advertise import Reports
-from connectors import redis_pool
+from connectors import redis_connections_pool
 
 
 async def sp_targets(amz: type(Reports), pool, report_date: date):
     api_url = '/v2/sp/targets/report'
     request_metrics = metrics['sp']['targets']
+    str_report_date = datetime.strftime(report_date, '%Y%m%d')
+
     body = {
         'metrics': ','.join(request_metrics),
-        'reportDate': datetime.strftime(report_date, '%Y%m%d')
+        'reportDate': str_report_date
     }
+    asyncio.sleep(0.5)
     logging.info('Request report')
     async with aiohttp.ClientSession() as session:
+        # TODO - add check if there were to many requests to the endpoint
         response = await amz.request(session=session, api_url=api_url, body=json.dumps(body))
-        logging.info('Response received, await response text')
-        response_text = await response.text()
+        # Read response content cause Amazon for this endpoint return incorrect Content-Type
+        response_text = await response.read()
+
+    # Convert content to dict and add report_date
+    response_data = json.loads(response_text)
+    report_id = response_data['reportId']
+    response_data.update({'report_date': datetime.strftime(report_date, '%Y-%m-%d')})
 
     with await pool as conn:
-        await conn.execute('LPUSH', 'targets:requested', response_text)
+        logging.info(f'Report {report_id} has been added to targets:requested')
+        # Add to the redis pool serialized json string
+        await conn.execute('LPUSH', 'targets:requested', json.dumps(response_data))
 
 
 async def main(period: int):
     amz = Reports()
-    pool = await redis_pool()
+    pool = await redis_connections_pool()
     date_list = [datetime.today()-timedelta(days=i) for i in range(period, 0, -1)]
     tasks = []
     for report_date in date_list:
@@ -39,4 +50,4 @@ async def main(period: int):
     await pool.wait_closed()
 
 if __name__ == '__main__':
-    asyncio.run(main(1))
+    asyncio.run(main(60))
